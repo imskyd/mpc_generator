9a8c8b1843c4f1cd68ac5ce0ea37a1367a13bb72
raven <losthorizon942@gmail.com>
2024-07-03 17:18:59 +0800

    wrap cobo err to normal err; add Approve function; fix get tx format err

--- go.mod
+++ go.mod
@@ -7,6 +7,12 @@ require github.com/CoboGlobal/cobo-go-api v0.46.0
 require (
 	github.com/bitly/go-simplejson v0.5.0 // indirect
 	github.com/btcsuite/btcd v0.0.0-20190824003749-130ea5bddde3 // indirect
+	github.com/btcsuite/btcd/btcec/v2 v2.2.0 // indirect
+	github.com/decred/dcrd/dcrec/secp256k1/v4 v4.0.1 // indirect
+	github.com/ethereum/go-ethereum v1.14.6 // indirect
+	github.com/holiman/uint256 v1.2.4 // indirect
 	github.com/kr/text v0.2.0 // indirect
 	github.com/rogpeppe/go-internal v1.12.0 // indirect
+	golang.org/x/crypto v0.22.0 // indirect
+	golang.org/x/sys v0.20.0 // indirect
 )
--- mpc.go
+++ mpc.go
@@ -1,9 +1,13 @@
 package mpc_generator
 
 import (
+	"encoding/json"
 	"fmt"
 	"github.com/CoboGlobal/cobo-go-api/cobo_custody"
+	"github.com/ethereum/go-ethereum/accounts/abi"
+	"github.com/ethereum/go-ethereum/common"
 	"math/big"
+	"strings"
 	"time"
 )
 
@@ -33,22 +37,41 @@ func (m *MPC) createRequestId() string {
 	return fmt.Sprintf("cs-go-%d", time.Now().UnixMilli())
 }
 
-func (m *MPC) Transfer(coin, from, to string, amount, gasPrice, gasLimit *big.Int) (string, *cobo_custody.ApiError) {
+func (m *MPC) Transfer(coin, from, to string, amount, gasPrice, gasLimit *big.Int) (string, error) {
 	requestId := m.createRequestId()
 	res, err := m.client.CreateTransaction(coin, requestId, amount, from, to, "", nil, gasPrice, gasLimit, OperationTransfer, "", nil, nil, nil, "")
 	if err != nil {
-		return "", err
+		return "", wrapCoboErr(err)
 	}
 	coboId, _ := res.Get("cobo_id").String()
 	return coboId, nil
 }
 
-func (m *MPC) ContractCall(coin, from, to, callData string, amount, gasPrice, gasLimit *big.Int) (string, *cobo_custody.ApiError) {
+func wrapCoboErr(coboErr *cobo_custody.ApiError) error {
+	if coboErr == nil {
+		return nil
+	}
+	j, _ := json.Marshal(coboErr)
+	return fmt.Errorf(string(j))
+}
+
+func (m *MPC) Approve(coin, from, token, spender string, approveAmount, gasPrice, gasLimit *big.Int) (string, error) {
+	abiString := "[{\"inputs\":[{\"internalType\":\"address\",\"name\":\"spender\",\"type\":\"address\"},{\"internalType\":\"uint256\",\"name\":\"amount\",\"type\":\"uint256\"}],\"name\":\"approve\",\"outputs\":[{\"internalType\":\"bool\",\"name\":\"\",\"type\":\"bool\"}],\"stateMutability\":\"nonpayable\",\"type\":\"function\"}]"
+	abiReader, _ := abi.JSON(strings.NewReader(abiString))
+	approvePack, packErr := abiReader.Pack("approve", common.HexToAddress(spender), approveAmount)
+	if packErr != nil {
+		return "", packErr
+	}
+	res, err := m.ContractCall(coin, from, token, fmt.Sprintf("0x%s", common.Bytes2Hex(approvePack)), big.NewInt(0), gasPrice, gasLimit)
+	return res, err
+}
+
+func (m *MPC) ContractCall(coin, from, to, callData string, amount, gasPrice, gasLimit *big.Int) (string, error) {
 	requestId := m.createRequestId()
 	extraParameters := fmt.Sprintf("{\"calldata\": \"%s\"}", callData)
 	res, err := m.client.CreateTransaction(coin, requestId, amount, from, to, "", nil, gasPrice, gasLimit, OperationContractCall, extraParameters, nil, nil, nil, "")
 	if err != nil {
-		return "", err
+		return "", wrapCoboErr(err)
 	}
 	coboId, _ := res.Get("cobo_id").String()
 	return coboId, nil
@@ -59,11 +82,26 @@ func (m *MPC) GetTransactionByRequestIds(requestIds string) ([]Transaction, erro
 	if err != nil {
 		return nil, fmt.Errorf(err.ErrorMessage)
 	}
-	array, err2 := res.Get("result").Get("transactions").Array()
-	var transactions []Transaction
-	for _, a := range array {
-		transactions = append(transactions, a.(Transaction))
+	txJson, err2 := res.Get("transactions").MarshalJSON()
+	if err != nil {
+		return nil, err2
 	}
+	var transactions []Transaction
+	_ = json.Unmarshal(txJson, &transactions)
+	return transactions, err2
+}
+
+func (m *MPC) GetTransactionByCoboIds(coboIds string) ([]Transaction, error) {
+	res, err := m.client.TransactionsByCoboIds(coboIds, 0)
+	if err != nil {
+		return nil, fmt.Errorf(err.ErrorMessage)
+	}
+	txJson, err2 := res.Get("transactions").MarshalJSON()
+	if err != nil {
+		return nil, err2
+	}
+	var transactions []Transaction
+	_ = json.Unmarshal(txJson, &transactions)
 	return transactions, err2
 }
 
