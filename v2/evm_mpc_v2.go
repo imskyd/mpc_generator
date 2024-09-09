f4652ce39e20bd3598d2de28f01da68bb0ee5203
raven <losthorizon942@gmail.com>
2024-09-09 13:41:33 +0800

    add evm mpcv2 function EstimateGas

--- v2/evm_mpc_v2.go
+++ v2/evm_mpc_v2.go
@@ -7,10 +7,15 @@ import (
 	"fmt"
 	coboWaas2 "github.com/CoboGlobal/cobo-waas2-go-sdk/cobo_waas2"
 	"github.com/CoboGlobal/cobo-waas2-go-sdk/cobo_waas2/crypto"
+	"github.com/ethereum/go-ethereum"
+	"github.com/ethereum/go-ethereum/common"
+	"github.com/ethereum/go-ethereum/ethclient"
+	"github.com/shopspring/decimal"
 	"github.com/sirupsen/logrus"
 	"io"
 	"log"
 	"net/http"
+	"strings"
 	"time"
 )
 
@@ -21,9 +26,10 @@ type EvmMpcV2 struct {
 	env        int
 	debug      bool
 	logger     *logrus.Logger
+	ethClient  *ethclient.Client
 }
 
-func NewEvmMpcV2(env int, privateKey, walletId string) *EvmMpcV2 {
+func NewEvmMpcV2(env int, privateKey, walletId, evmRpc string) *EvmMpcV2 {
 	if env != coboWaas2.ProdEnv && env != coboWaas2.DevEnv {
 		log.Panic("env should be coboWaas2.ProdEnv or coboWaas2.DevEnv")
 	}
@@ -48,6 +54,9 @@ func NewEvmMpcV2(env int, privateKey, walletId string) *EvmMpcV2 {
 		TimestampFormat: time.StampMilli,
 	})
 	mpc.logger = logger
+
+	ethClient, _ := ethclient.Dial(evmRpc)
+	mpc.ethClient = ethClient
 
 	return &mpc
 }
@@ -85,4 +94,34 @@ func (m *EvmMpcV2) formatResponse(resp *coboWaas2.CreateTransferTransaction201Re
 		return nil, fmt.Errorf("err: %s, unable to read response body: %s", err.Error(), readErr)
 	}
 	return nil, fmt.Errorf("err: %s, body: %s", err.Error(), string(body))
+}
+
+func (m *EvmMpcV2) EstimateGas(from, to, data, value, gasPrice, maxFee, priorityGas string) (uint64, error) {
+	fromAddr := common.HexToAddress(from)
+	toAddr := common.HexToAddress(to)
+	if strings.HasPrefix(data, "0x") {
+		data = data[2:]
+	}
+	dataHex := common.FromHex(data)
+	gasPriceBigInt, _ := decimal.NewFromString(gasPrice)
+	maxFeeBigInt, _ := decimal.NewFromString(maxFee)
+	priorityGasBigInt, _ := decimal.NewFromString(priorityGas)
+
+	valueBigInt, _ := decimal.NewFromString(value)
+	mul := decimal.NewFromFloat(float64(10)).Pow(decimal.NewFromFloat(float64(18)))
+	result := valueBigInt.Mul(mul)
+
+	msg := ethereum.CallMsg{
+		From:      fromAddr,
+		To:        &toAddr,
+		Gas:       0,
+		GasPrice:  gasPriceBigInt.BigInt(),
+		GasFeeCap: maxFeeBigInt.BigInt(),
+		GasTipCap: priorityGasBigInt.BigInt(),
+		Value:     result.BigInt(),
+		Data:      dataHex,
+	}
+
+	gas, err := m.ethClient.EstimateGas(context.Background(), msg)
+	return gas, err
 }
--- v2/example/flow.go
+++ v2/example/flow.go
@@ -1,10 +1,13 @@
 package example
 
 import (
+	"context"
 	coboWaas2 "github.com/CoboGlobal/cobo-waas2-go-sdk/cobo_waas2"
 	"github.com/ethereum/go-ethereum/common"
+	"github.com/ethereum/go-ethereum/ethclient"
 	"github.com/imskyd/mpc_generator/base"
 	v2 "github.com/imskyd/mpc_generator/v2"
+	"github.com/shopspring/decimal"
 	"github.com/sirupsen/logrus"
 	"math/big"
 	"time"
@@ -17,9 +20,10 @@ const env = coboWaas2.ProdEnv
 const privateKey = "<PRIVATE_KEY>"
 const walletId = "<WALLET_ID>"
 const tokenId = "<TOKEN_ID>"
+const rpc = "<RPC_URL>"
 
 func init() {
-	mpc = v2.NewEvmMpcV2(env, privateKey, walletId)
+	mpc = v2.NewEvmMpcV2(env, privateKey, walletId, rpc)
 
 	logger = logrus.New()
 	logger.SetFormatter(&logrus.TextFormatter{
@@ -28,11 +32,21 @@ func init() {
 	})
 }
 
+func MulGasPrice(gasPrice *big.Int, mul float64) *big.Int {
+	mulGasPriceInit, _ := decimal.NewFromString(gasPrice.String())
+	mulGasPrice := mulGasPriceInit.Mul(decimal.NewFromFloat(mul))
+	return mulGasPrice.BigInt()
+}
+
 func main() {
 	execAddress := []string{"address1", "address2"}
+	client, _ := ethclient.Dial("<RPC_URL>")
 
 	for _, eAddr := range execAddress {
-		fee := v2.New1559Fee(tokenId, "300", "100", "21000")
+		gasPrice, err := client.SuggestGasPrice(context.Background())
+		maxFee := MulGasPrice(gasPrice, 2)
+		priority := MulGasPrice(gasPrice, 0.1)
+		fee := v2.New1559Fee(tokenId, maxFee.String(), priority.String(), "27000")
 		//approve token
 		response, err := mpc.TokenApprove(tokenId, eAddr, "<TOKEN_ADDRESS>", "<SPENDER_ADDRESS>", "3000", &fee)
 		if err != nil {
@@ -44,9 +58,17 @@ func main() {
 			return
 		}
 
-		fee2 := v2.New1559Fee(tokenId, "300", "100", "21000")
+		gasPrice2, _ := client.SuggestGasPrice(context.Background())
+		maxFee2 := MulGasPrice(gasPrice2, 2)
+		priority2 := MulGasPrice(gasPrice2, 0.1)
 		//contract interact
 		callData, _ := base.GetAbiCallData("abi", "transfer", common.HexToAddress("address"), big.NewInt(0))
+		gasLimit2, err := mpc.EstimateGas(eAddr, "<TOKEN_ADDRESS>", callData, "0.00001", "", maxFee2.String(), priority2.String())
+		if err != nil {
+			logger.Errorf("Failed to approve token: %s", err.Error())
+			return
+		}
+		fee2 := v2.New1559Fee(tokenId, maxFee2.String(), priority2.String(), decimal.NewFromUint64(gasLimit2).String())
 		callResp1, callErr1 := mpc.ContractCall(tokenId, eAddr, "<TOKEN_ADDRESS>", callData, "0.00001", &fee2)
 		if callErr1 != nil {
 			logger.Errorf("Failed to approve token: %s", callErr1.Error())
